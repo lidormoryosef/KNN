1d9b20167941fb216b3e047e14982c6a6194d95d
omer <omerbadichi210298@gmail.com>
2023-01-18 13:34:29 +0200

    creating global data

---
+++ global_data.cpp
@@ -0,0 +1,9 @@
+//
+// Created by omer on 1/18/23.
+//
+
+#include "global_data.h"
+
+void global_data::setClassified(vector<NameVector> classified) {
+
+}
---
+++ global_data.h
@@ -0,0 +1,33 @@
+//
+// Created by omer on 1/18/23.
+//
+
+#ifndef EX_4_GLOBAL_DATA_H
+#define EX_4_GLOBAL_DATA_H
+
+
+#include "server/ClassifiedArray.h"
+
+class global_data {
+private:
+    int k;
+    ClassifiedArray classifiedArray;
+    vector<NameVector> unClassifiedArray;
+    Distance* norm;
+    string name_norm;
+public:
+    void setClassified(vector<NameVector> classified);
+    void setUnClassified(vector<NameVector> unClassified);
+    void setK(int k);
+    void setNorm(Distance* dis);
+    void setNameNorm(string name);
+    ClassifiedArray getClassified();
+    vector<NameVector> getUnClassified();
+    int getK();
+    Distance* getNorm();
+    string getNameNorm();
+    global_data();
+};
+
+
+#endif //EX_4_GLOBAL_DATA_H
